b8b22a674b8b368699a0fd6252d500cfe16e810c
nprofilovic <nprofilovic@gmail.com>
2019-11-19 13:02:09 +0100

    Upload first files

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,25 @@
+<template>
+    <form>
+        <input type="text" name="title" placeholder="Add Todo...">
+        <input type="submit" value="Submit" class="btn">
+    </form>
+</template>
+
+<script>
+export default {
+    name: "AddTodo",
+}
+</script>
+
+<style scoped>
+    form {
+        display: flex;
+    }
+    input[type="text"] {
+        flex:10;
+        padding: 5px;
+    }
+    input[type="submit"] {
+        flex:2
+    }
+</style>
